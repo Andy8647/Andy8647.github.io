9b540d07e314300caa238be33281d6b8ab32d39f
Andy8647 <andy8647lgc@gmail.com>
2021-03-08 15:49:45 -0500

    type animation finished

--- js/typeEffect.js
+++ js/typeEffect.js
@@ -1,4 +1,5 @@
 const objectText = document.getElementById("typeEffect");
+// console.log(objectText)
 
 objectText.innerHTML = objectText.textContent
   .replace(/\S/g, "<span>$&</span>")
@@ -7,6 +8,13 @@ objectText.innerHTML = objectText.textContent
 let delay = 0;
 document.querySelectorAll("span").forEach((span, index) => {
   delay += 0.1;
-
+  if (index === 10) delay += 0.3
   span.style.setProperty("--delay", `${delay}s`);
 });
+
+objectText.addEventListener('animationend', (e) => {
+    if (e.target === document.querySelector('#typeEffect span:last-child')) {
+        console.log(e.target)
+      objectText.classList.add('ended')
+    }
+  })
